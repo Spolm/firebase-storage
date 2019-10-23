c4dc47f160700952ee70131eadd19eaf8ff93839
Luis Machado <luismachadoz.12@gmail.com>
2019-10-23 13:18:35 -0400

    clase imagen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Image } from './image';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UploadTest';
+  ref: any;
+  image: Image;
+  uploadProgress: any;
+  downloadURL: any;
+  constructor(private afStorage: AngularFireStorage) { }
+  upload(event) {
+    // create a random id
+    const randomId = Math.random().toString(36).substring(2);
+    // create a reference to the storage bucket location
+    this.ref = this.afStorage.ref(randomId);
+    // the put method creates an AngularFireUploadTask
+    // and kicks off the upload
+    this.image = this.ref.put(event.target.files[0]);
+    this.uploadProgress = this.ref.percentageChanges();
+    this.downloadURL = this.ref.downloadURL();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDI9P9RV77HorL1o0tTXZ6dwCtMD1LIA4s",
+      authDomain: "uploadtest-a0ebc.firebaseapp.com",
+      databaseURL: "https://uploadtest-a0ebc.firebaseio.com",
+      projectId: "uploadtest-a0ebc",
+      storageBucket: "uploadtest-a0ebc.appspot.com",
+      messagingSenderId: "928110400155",
+      appId: "1:928110400155:web:9e3d46a3e9fdab70e0ac3e"
+   }),
+   AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
