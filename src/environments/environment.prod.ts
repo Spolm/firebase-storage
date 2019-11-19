fbab512aa31dc7ff21ce60b50c525bbbf36b4ae1
Luis Machado <luismachadoz.12@gmail.com>
2019-11-19 15:53:42 -0400

    cambios para firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDI9P9RV77HorL1o0tTXZ6dwCtMD1LIA4s",
+    authDomain: "uploadtest-a0ebc.firebaseapp.com",
+    databaseURL: "https://uploadtest-a0ebc.firebaseio.com",
+    projectId: "uploadtest-a0ebc",
+    storageBucket: "uploadtest-a0ebc.appspot.com",
+    messagingSenderId: "928110400155",
+    appId: "1:928110400155:web:9e3d46a3e9fdab70e0ac3e"
+  },
+  emailAPI: 'http://xpik.io',
+  database: 'firebase',
+  social: {
+    fblink: 'https://www.facebook.com/xpik',
+    linkedin: 'https://www.linkedin.com/in/xpik/',
+    github: '',
+    emailid: 'info@xpik.io',
+    homelink: ''
+  },
+  socialAuthEnabled: true,
+  graphql: 'http://localhost:3000/graphql'
+};
 };
